240887da2347e86d73714e0e98a54e60604ff035
qzy <weelgod@gmail.com>
2018-03-07 08:28:24 +0800

    2-72

--- block.js
+++ block.js
@@ -2,16 +2,15 @@
  * Created by qzy on 2018/3/5.
  * File description:
  */
-var Block = function () {
+var Block = function (position) {
   var image = imageFromPath('block.png')
-  image.onload = function () {
-    o.w = this.width
-    o.h = this.height
-  }
+
   var o = {
     image: image,
-    x: 100,
-    y: 100,
+    w:image.width,
+    h:image.height,
+    x: position[0],
+    y: position[1],
     alive: true,
     kill: function () {
       o.alive = false
